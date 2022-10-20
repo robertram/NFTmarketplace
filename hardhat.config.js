523843991620bfd264b85aedfd31aa15ead54741
Robert Ramirez <rsft6000@gmail.com>
2022-10-19 20:11:49 -0600

    contract and hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
-const fs = require('fs');
+const fs = require("fs");
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+require("dotenv").config();
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -15,20 +16,20 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
     goerli: {
-      url: "<YOUR_ALCHEMY_URL>",
-      accounts: [ "<YOUR_PRIVATE_KEY>" ]
-    }
+      url: process.env.REACT_APP_ALCHEMY_API_URL,
+      accounts: [process.env.REACT_APP_PRIVATE_KEY],
+    },
   },
   solidity: {
     version: "0.8.4",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
-  }
+        runs: 200,
+      },
+    },
+  },
 };
