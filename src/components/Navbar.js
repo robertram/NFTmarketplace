56a8682dcc9896e1b9cc2606cdf054253f910394
Robert Ramirez <rsft6000@gmail.com>
2022-11-04 22:48:57 -0600

    login and list nfts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,75 +1,158 @@
-import logo from '../logo_3.png';
-import fullLogo from '../full_logo.png';
+import fullLogo from "../images/full_logo.png";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
   useRouteMatch,
-  useParams
+  useParams,
 } from "react-router-dom";
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router';
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
 
 function Navbar() {
+  const [connected, toggleConnect] = useState(false);
+  const location = useLocation();
+  const [currAddress, updateAddress] = useState("0x");
 
-const [connected, toggleConnect] = useState(false);
-const location = useLocation();
-const [currAddress, updateAddress] = useState('0x');
+  async function getNFTData() {
+    const ethers = require("ethers");
 
-    return (
-      <div className="">
-        <nav className="w-screen">
-          <ul className='flex items-end justify-between py-3 bg-transparent text-white pr-5'>
-          <li className='flex items-end ml-5 pb-2'>
+    //After adding your Hardhat network to your metamask, this code will get providers and signers
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const addr = await signer.getAddress();
+
+    console.log("addr", addr);
+
+    updateAddress(addr);
+  }
+
+  async function getAddress() {
+    const ethers = require("ethers");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const addr = await signer.getAddress();
+    updateAddress(addr);
+  }
+
+  const updateButton = () => {
+    const ethereumButton = document.querySelector(".enableEthereumButton");
+    ethereumButton.textContent = "Connected";
+    ethereumButton.classList.remove("hover:bg-blu-70");
+    ethereumButton.classList.remove("bg-blu-500");
+    ethereumButton.classList.add("hover:bg-green-70");
+    ethereumButton.classList.add("bg-green-500");
+  };
+
+  async function connectWebsite() {
+    if (connected) {
+      await window.ethereum.request({
+        method: "eth_requestAccounts",
+        params: [{ eth_accounts: {} }],
+      });
+    }
+    const chainId = await window.ethereum.request({ method: "eth_chainId" });
+    if (chainId !== "0x5") {
+      //alert('Incorrect network! Switch-your netamask network to Rinkeby'):
+      await window.ethereum.request({
+        method: "wallet_switchEthereunChain",
+        params: [{ chainId: "0x5" }],
+      });
+    }
+    await window.ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then(() => {
+        updateButton();
+        console.log("here");
+        getAddress();
+        window.location.replace(location.pathname);
+      });
+  }
+
+  useEffect(() => {
+    let val = window.ethereum.isConnected();
+    if (val) {
+      console.log("is it because of this?", val);
+      getAddress();
+      toggleConnect(val);
+      updateButton();
+    }
+    window.ethereum.on("accountsChanged", function (accounts) {
+      window.location.replace(location.pathname);
+    });
+  }, [location.pathname]);
+
+  const params = useParams();
+  const tokenId = params.tokenId;
+
+  return (
+    <div className="">
+      <nav className="w-screen">
+        <ul className="flex items-end justify-between py-3 bg-transparent text-white pr-5">
+          <li className="flex items-end ml-5 pb-2">
             <Link to="/">
-            <img src={fullLogo} alt="" width={120} height={120} className="inline-block -mt-2"/>
-            <div className='inline-block font-bold text-xl ml-2'>
-              NFT Marketplace
-            </div>
+              <img
+                src={fullLogo}
+                alt=""
+                width={120}
+                height={120}
+                className="inline-block -mt-2"
+              />
+              <div className="inline-block font-bold text-xl ml-2">
+                NFT Marketplace
+              </div>
             </Link>
           </li>
-          <li className='w-2/6'>
-            <ul className='lg:flex justify-between font-bold mr-10 text-lg'>
-              {location.pathname === "/" ? 
-              <li className='border-b-2 hover:pb-0 p-2'>
-                <Link to="/">Marketplace</Link>
-              </li>
-              :
-              <li className='hover:border-b-2 hover:pb-0 p-2'>
-                <Link to="/">Marketplace</Link>
-              </li>              
-              }
-              {location.pathname === "/sellNFT" ? 
-              <li className='border-b-2 hover:pb-0 p-2'>
-                <Link to="/sellNFT">List My NFT</Link>
-              </li>
-              :
-              <li className='hover:border-b-2 hover:pb-0 p-2'>
-                <Link to="/sellNFT">List My NFT</Link>
-              </li>              
-              }              
-              {location.pathname === "/profile" ? 
-              <li className='border-b-2 hover:pb-0 p-2'>
-                <Link to="/profile">Profile</Link>
-              </li>
-              :
-              <li className='hover:border-b-2 hover:pb-0 p-2'>
-                <Link to="/profile">Profile</Link>
-              </li>              
-              }  
+          <li className="w-2/6">
+            <ul className="lg:flex justify-between font-bold mr-10 text-lg">
+              {location.pathname === "/" ? (
+                <li className="border-b-2 hover:pb-0 p-2">
+                  <Link to="/">Marketplace</Link>
+                </li>
+              ) : (
+                <li className="hover:border-b-2 hover:pb-0 p-2">
+                  <Link to="/">Marketplace</Link>
+                </li>
+              )}
+              {location.pathname === "/sellNFT" ? (
+                <li className="border-b-2 hover:pb-0 p-2">
+                  <Link to="/sellNFT">List My NFT</Link>
+                </li>
+              ) : (
+                <li className="hover:border-b-2 hover:pb-0 p-2">
+                  <Link to="/sellNFT">List My NFT</Link>
+                </li>
+              )}
+              {location.pathname === "/profile" ? (
+                <li className="border-b-2 hover:pb-0 p-2">
+                  <Link to="/profile">Profile</Link>
+                </li>
+              ) : (
+                <li className="hover:border-b-2 hover:pb-0 p-2">
+                  <Link to="/profile">Profile</Link>
+                </li>
+              )}
               <li>
-                <button className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm">{connected? "Connected":"Connect Wallet"}</button>
+                <button
+                  className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm"
+                  onClick={connectWebsite}
+                >
+                  {connected ? "Connected" : "Connect Wallet"}
+                </button>
               </li>
             </ul>
           </li>
-          </ul>
-        </nav>
-        <div className='text-white text-bold text-right mr-10 text-sm'>
-          {currAddress !== "0x" ? "Connected to":"Not Connected. Please login to view NFTs"} {currAddress !== "0x" ? (currAddress.substring(0,15)+'...'):""}
-        </div>
+        </ul>
+      </nav>
+      <div className="text-white text-bold text-right mr-10 text-sm">
+        {currAddress !== "0x"
+          ? "Connected to"
+          : "Not Connected. Please login to view NFTs"}{" "}
+        {currAddress !== "0x" ? currAddress.substring(0, 15) + "..." : ""}
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export default Navbar;
+export default Navbar;
